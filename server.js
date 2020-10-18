77e27b40d4a1d7e9439ad550dc19df778d085967
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-10-18 16:40:57 -0700

    updated server js to include routes and connecting to db

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const mongoose = require ('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGOD_URI || 'mongod://localhost/workout', {
+useNewUrlParser: true,
+useFindAndModify: false
+});
+
+app.use(require('./routes/api.js'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
+
