54ef682bcddaeb1d8b5a30c24a11c05ba67dc7b8
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-10-25 16:23:00 -0700

    deploying

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost/workout', {
-useNewUrlParser: true,
-useFindAndModify: false
+mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost/workout-exercise', 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 app.use(logger('dev'));
