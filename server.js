39d3a76a7aafef748bf311c1b18c48dbb69f769e
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-10-25 18:13:47 -0700

    updated server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost/workout-exercise', 
-{
+mongoose.connect(process.env.MONGOD_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
